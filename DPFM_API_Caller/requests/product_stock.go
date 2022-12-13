feaa83ea5c8f0ff3ecc87eb41857868cee42cfad
MiyuYokoyama0109 <miyu.y@latona.io>
2022-12-13 15:46:01 +0900

    release

--- DPFM_API_Caller/requests/product_stock.go
+++ DPFM_API_Caller/requests/product_stock.go
@@ -1,15 +1,15 @@
 package requests
 
 type ProductStock struct {
-	BusinessPartner           *int     `json:"BusinessPartner"`
+	BusinessPartner           int      `json:"BusinessPartner"`
 	Product                   string   `json:"Product"`
 	Plant                     string   `json:"Plant"`
-	StorageLocation           string   `json:"StorageLocation"`
-	Batch                     string   `json:"Batch"`
+	StorageLocation           *string  `json:"StorageLocation"`
+	Batch                     *string  `json:"Batch"`
 	OrderID                   *int     `json:"OrderID"`
 	OrderItem                 *int     `json:"OrderItem"`
-	Project                   string   `json:"Project"`
-	InventoryStockType        string   `json:"InventoryStockType"`
-	InventorySpecialStockType string   `json:"InventorySpecialStockType"`
+	Project                   *string  `json:"Project"`
+	InventoryStockType        *string  `json:"InventoryStockType"`
+	InventorySpecialStockType *string  `json:"InventorySpecialStockType"`
 	ProductStock              *float32 `json:"ProductStock"`
 }
--- DPFM_API_Input_Reader/type.go
+++ DPFM_API_Input_Reader/type.go
@@ -70,31 +70,31 @@ type SDC struct {
 }
 
 type ProductStock struct {
-	BusinessPartner           *int                     `json:"BusinessPartner"`
+	BusinessPartner           int                      `json:"BusinessPartner"`
 	Product                   string                   `json:"Product"`
 	Plant                     string                   `json:"Plant"`
-	StorageLocation           string                   `json:"StorageLocation"`
-	Batch                     string                   `json:"Batch"`
+	StorageLocation           *string                  `json:"StorageLocation"`
+	Batch                     *string                  `json:"Batch"`
 	OrderID                   *int                     `json:"OrderID"`
 	OrderItem                 *int                     `json:"OrderItem"`
-	Project                   string                   `json:"Project"`
-	InventoryStockType        string                   `json:"InventoryStockType"`
-	InventorySpecialStockType string                   `json:"InventorySpecialStockType"`
+	Project                   *string                  `json:"Project"`
+	InventoryStockType        *string                  `json:"InventoryStockType"`
+	InventorySpecialStockType *string                  `json:"InventorySpecialStockType"`
 	ProductStock              *float32                 `json:"ProductStock"`
 	ProductStockAvailability  ProductStockAvailability `json:"ProductStockAvailability"`
 }
 
 type ProductStockAvailability struct {
-	BusinessPartner              *int     `json:"BusinessPartner"`
+	BusinessPartner              int      `json:"BusinessPartner"`
 	Product                      string   `json:"Product"`
 	Plant                        string   `json:"Plant"`
-	Batch                        string   `json:"Batch"`
+	Batch                        *string  `json:"Batch"`
 	BatchValidityEndDate         *string  `json:"BatchValidityEndDate"`
 	OrderID                      *int     `json:"OrderID"`
 	OrderItem                    *int     `json:"OrderItem"`
-	Project                      string   `json:"Project"`
-	InventoryStockType           string   `json:"InventoryStockType"`
-	InventorySpecialStockType    string   `json:"InventorySpecialStockType"`
-	ProductStockAvailabilityDate *string  `json:"ProductStockAvailabilityDate"`
+	Project                      *string  `json:"Project"`
+	InventoryStockType           *string  `json:"InventoryStockType"`
+	InventorySpecialStockType    *string  `json:"InventorySpecialStockType"`
+	ProductStockAvailabilityDate string   `json:"ProductStockAvailabilityDate"`
 	AvailableProductStock        *float32 `json:"AvailableProductStock"`
 }
